dbb58243c3a02a44406f419d0b80f95bbd9e3650
Mat911 <matisse.demontis@gmail.com>
2025-05-15 13:38:59 +0400

    Create OpenApiConfig.java

---
+++ src/main/java/com/example/api_gestion_de_taches_demontis/Config/OpenApiConfig.java
@@ -0,0 +1,29 @@
+package com.example.api_gestion_de_taches_demontis.Config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(new Info()
+                        .title("API de gestion de tâches")
+                        .description("API REST permettant la gestion des tâches, projets, catégories et utilisateurs")
+                        .version("1.0")
+                        .contact(new Contact()
+                                .name("Demontis")
+                                .email("contact@example.com"))
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("http://www.apache.org/licenses/LICENSE-2.0.html")));
+    }
+}
