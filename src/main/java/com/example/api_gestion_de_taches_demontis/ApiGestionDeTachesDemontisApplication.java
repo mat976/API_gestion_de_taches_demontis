b1509b2fb148f0cd42429f47ec5ad94ba7002684
Mat911 <matisse.demontis@gmail.com>
2025-05-15 11:09:08 +0400

    Initial commit

---
+++ src/main/java/com/example/api_gestion_de_taches_demontis/ApiGestionDeTachesDemontisApplication.java
@@ -0,0 +1,13 @@
+package com.example.api_gestion_de_taches_demontis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGestionDeTachesDemontisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGestionDeTachesDemontisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/api_gestion_de_taches_demontis/ApiGestionDeTachesDemontisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.api_gestion_de_taches_demontis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGestionDeTachesDemontisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
